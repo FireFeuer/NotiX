ae2b807605482f18a4d3d0049311d1d9f81ad5fc
user_12_19P1 <muhametova.alcu@example.com>
2023-02-27 14:04:21 +0500

    Новое

--- NotiX7/NotiX7/MainWindow.xaml.cs
+++ NotiX7/NotiX7/MainWindow.xaml.cs
@@ -17,7 +17,7 @@ namespace NotiX7
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
-    /// ggsgsesegs
+    /// Илья
     /// </summary>
     public partial class MainWindow : Window
     {
