e3f7973605333bf37158bfceb825c0e6bd142aae
user_12_19P1 <muhametova.alcu@example.com>
2023-03-20 15:05:24 +0500

    Сделана основная структура программы

---
+++ NotiX7/NotiX7/Data/DbEntities/ColorsCategory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NotiX7;
+
+public partial class ColorsCategory
+{
+    public long Id { get; set; }
+
+    public string? Text { get; set; }
+
+    public string? Hex { get; set; }
+
+    public virtual ICollection<Note> Notes { get; } = new List<Note>();
+}
---
+++ NotiX7/NotiX7/ServicesLocator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotiX7
+{
+    internal class ServicesLocator
+    {
+    }
+}
