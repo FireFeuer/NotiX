3fdcb0371717ecf443d9cbeec4333968d0ef8992
user_12_19P1 <muhametova.alcu@example.com>
2023-03-20 15:54:29 +0500

    Update 0 Добаленна viewmodel

--- NotiX7/NotiX7/ServicesLocator.cs
+++ NotiX7/NotiX7/ServicesLocator.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using NotiX7.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,22 @@ namespace NotiX7
 {
     internal class ServicesLocator
     {
+        public static ServiceProvider provider;
+
+        public static void Init()
+        {
+            ServiceCollection services = new ServiceCollection();
+
+            services.AddTransient<WindowViewModel>();
+           
+            provider = services.BuildServiceProvider();
+            foreach (var service in services)
+            {
+                provider.GetRequiredService(service.ServiceType);
+            }
+
+        }
+       
+        public WindowViewModel WindowViewModel => provider.GetRequiredService<WindowViewModel>();
     }
 }
