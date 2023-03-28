7bd0e41e44959d896684495e82e42ae7889cc62f
ShemenV <demonethdr@gmail.com>
2023-03-28 23:26:27 +0500

    Update 5
    Версия сворачивания с CheckBox

--- NotiX7/NotiX7/Views/UserControls/Note.xaml.cs
+++ NotiX7/NotiX7/Views/UserControls/Note.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows.Controls;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace NotiX7.Views.UserControls
 {
@@ -23,6 +11,11 @@ namespace NotiX7.Views.UserControls
         public NOte()
         {
             InitializeComponent();
+        }
+
+        private void StackPanel_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            expandCheckBox.IsChecked = !expandCheckBox.IsChecked;
         }
     }
 }
