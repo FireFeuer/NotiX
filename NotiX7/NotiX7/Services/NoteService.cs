bf07b6aaca1ffe20efe335b94e4b1b36858b5008
FireFeuer <aidar.bictimirov2017@yandex.ru>
2023-04-09 16:48:47 +0500

    UPDATE 17

--- NotiX7/NotiX7/Services/NoteService.cs
+++ NotiX7/NotiX7/Services/NoteService.cs
@@ -1,8 +1,14 @@
 ﻿using NotiX7.Data;
 using NotiX7.Data.DbEntities;
 using NotiX7.Models;
+using NotiX7.Views.UserControls;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Sqlite;
+using static System.Net.Mime.MediaTypeNames;
+using System.Windows;
+using Microsoft.EntityFrameworkCore;
 
 namespace NotiX7.Services
 {
@@ -26,13 +32,72 @@ namespace NotiX7.Services
                         Text = note.Text,
                         FirstDate = note.FirstDate,
                         SecondDate = note.SecondDate,
-                        ColorNavigation = note.ColorNavigation
+                        ColorNavigation = note.ColorNavigation,
+                        Is_open = note.Is_open,
+                        Size = note.Size
                     };
                     notes.Add(addedNote);
                 }
             }
             return notes;
+        }
 
+        public async Task AddUploadingNotesToTheDb(Note note)
+        {
+            using (NotixDbContext db = new NotixDbContext())
+            {              
+                Microsoft.EntityFrameworkCore.DbSet<NoteDB> notesDB;
+                notesDB = db.Notes;
+                notesDB.Add(new NoteDB { 
+                       Title = note.Title,
+                       Text = note.Text,
+                       FirstDate = note.FirstDate,
+                       SecondDate = note.SecondDate,
+                       Size = note.Size,
+                       Color = note.Color,
+                       X = note.X,
+                       Y = note.Y,
+                       Is_open = note.Is_open,
+                });
+                db.SaveChanges();
+            }
+        }
+        public async Task ChangeUploadingNotesToTheDb(Note note)
+        {
+            using (NotixDbContext db = new NotixDbContext())
+            {
+                foreach(var noteDB in db.Notes)
+                {
+                    if(note.Title == noteDB.Title)
+                    {
+
+                        //noteDB.Title = "ASD";
+                        //noteDB.Text = "SAD";
+                        //noteDB.FirstDate = "11/11/1111";
+                        //noteDB.SecondDate = "12/12/1200";
+                        //noteDB.Size = 1;
+                        //noteDB.Color = 1;
+                        //noteDB.X = 111;
+                        //noteDB.Y = 124;
+                        //noteDB.Is_open = 1;
+                        noteDB.Title = note.Title;
+                        noteDB.Text = note.Text;
+                        noteDB.FirstDate = note.FirstDate;
+                        noteDB.SecondDate = note.SecondDate;
+                        noteDB.Size = note.Size;
+                        noteDB.Color = 1;
+                        noteDB.X = note.X;
+                        noteDB.Y = note.Y;
+                        noteDB.Is_open = note.Is_open;
+                        MessageBox.Show("sad");
+                        MessageBox.Show(noteDB.Id.ToString());
+                    }
+                     
+                }
+             
+                db.SaveChanges();
+                
+            }
         }
     }
 
